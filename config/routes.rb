291f3bc65dc7b33d7de2e0c47f09ff6354d9b9d5
eversonclear <everson.pereira.clear@gmail.com>
2023-12-03 15:37:11 -0300

    fix: removing useless endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :usuarios, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    registration: 'signup'
   },
   controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  }, skip: [:passwords, :registrations]
 
   # usuarios
   post '/visitantes', to: 'usuarios#criar_visitante'
