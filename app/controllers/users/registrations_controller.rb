9b8cb34458c8cad99e072b9c6160ea19e2257ef4
eversonclear <everson.pereira.clear@gmail.com>
2023-12-03 15:36:48 -0300

    fix: using user jbuilder

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,10 +15,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def respond_with(current_user, _opts = {})
     if resource.persisted?
-      render json: {
-        status: {code: 200, message: 'Signed up successfully.'},
-        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
-      }
+      @usuario = current_user
+      render 'usuarios/show.json.jbuilder', status: :ok
     else
       render json: {
         status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,12 +2,8 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
   private
   def respond_with(current_user, _opts = {})
-    render json: {
-      status: { 
-        code: 200, message: 'Logged in successfully.',
-        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
-      }
-    }, status: :ok
+    @usuario = current_user
+    render 'usuarios/show.json.jbuilder', status: :ok
   end
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
